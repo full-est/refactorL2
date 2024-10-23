48584f8565b3060f923dab7a2a39f7be1615ddb9
FahimFBA <fahimbinamin@gmail.com>
2024-10-23 13:26:01 +0600

    planning - in comments

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# Get all todos
+
+# Get single todo
+
+# Create a todo
+
+# Update a todo
+
+# Delete a todo
